0f6c35b8f723eeef4f4e9c6e050cae149e5a9b35
mundstock <70247406+JorkDev@users.noreply.github.com>
2024-09-17 20:37:22 -0500

    Added input buttons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ const App = () => {
         <p>What do you want to know?
           <button className="surprise">Surprise me</button>
         </p>
+        <div className="input-container">
+          <input value={""} 
+          placeholder="When is Christmas...?" 
+          onChange={""} />
+            <button>Ask Me</button>
+            <button>Clear</button>
+        </div>
       </section>
     </div>
   )
