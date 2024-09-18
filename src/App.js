04e563980d660994625d720e69ffae0cb382bc99
mundstock <70247406+JorkDev@users.noreply.github.com>
2024-09-17 21:30:52 -0500

    Refactor and error handling with conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import { useState } from "react"
+
+
 const App = () => {
+  const [error, setError] = useState("there was an error")
+
   return (
     <div className="app">
-      <section className="search-section">
-        <p>What do you want to know?
-          <button className="surprise">Surprise me</button>
-        </p>
-        <div className="input-container">
-          <input value={""} 
-          placeholder="When is Christmas...?" 
-          onChange={""} />
-            <button>Ask Me</button>
-            <button>Clear</button>
+      <p>What do you want to know?
+        <button className="surprise">Surprise me</button>
+      </p>
+      <div className="input-container">
+        <input value={""}
+          placeholder="When is Christmas...?"
+          onChange={""}
+        />
+        {!error && <button>Ask Me</button>}
+        {error && <button>Clear</button>}
+      </div>
+      {error && <p>{error}</p>}
+      <div className="search-result">
+        <div key={""}>
+          <p className="answer"></p>
         </div>
-      </section>
+      </div>
     </div>
   )
 }
